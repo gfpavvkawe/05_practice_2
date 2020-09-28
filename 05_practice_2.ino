0d2b05129df72f86a1268f8ba98c26df390ab8e1
Song Taekmoo <ewakvvapfg@kookmin.ac.kr>
2020-09-28 15:48:35 +0900

    조건에 맞게 수정

--- 05_practice_2.ino
+++ 05_practice_2.ino
@@ -1,16 +1,18 @@
+#define LED_PIN 7
+
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  for (int i = 0; i < 5; i++) {
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(100);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(100);
-  }
-  digitalWrite(LED_BUILTIN, LOW);
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
+  digitalWrite(LED_PIN, LOW);
+  delay(1000);
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(LED_PIN, HIGH);
+    delay(100);
+    digitalWrite(LED_PIN, LOW);
+    delay(100);
+  }
+  digitalWrite(LED_PIN, HIGH);
   while(1);
 }
